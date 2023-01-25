17517272d2eb4144f12c8f78592bd530cf1370fd
kitdog123 <kitty_purcell@btinternet.com>
2023-01-25 09:54:22 +0000

    Added link to git as a footer

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+      {" "}
+      <a href="https://github.com/kitdog123/react-weather-app-1">
+        Open source code
+      </a>{" "}
+      by Kitty Purcell.{" "}
+    </footer>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Search from "./Search";
+import Footer from "./Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <Search />
+    <Footer />
   </React.StrictMode>
 );
