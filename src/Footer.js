1a1469ef19cc03976815d0cfd7998656a8b00bdf
kitdog123 <kitty_purcell@btinternet.com>
2023-02-02 10:52:19 +0000

    Footer and footer.css

--- src/Footer.js
+++ src/Footer.js
@@ -1,34 +1,27 @@
+import "./Footer.css";
+
 export default function Footer() {
   return (
     <footer>
-      {" "}
-      <a href="https://github.com/kitdog123/react-weather-app-1">
-        Open source code
-      </a>{" "}
-      by Kitty Purcell.{" "}
+      <a
+        className="footer-link"
+        href="https://github.com/kitdog123/react-weather-app-1"
+      >
+        Open Source Code
+      </a>
+      by Kitty <br />
+      Hosted on
+      <a className="footer-link" href="https://www.netlify.com/">
+        Netlify
+      </a>
+      <br />
+      Animated icons by
+      <a
+        className="footer-link"
+        href="https://www.flaticon.com/free-animated-icons/weather"
+      >
+        Flat Icon
+      </a>
     </footer>
   );
 }
-
-
-<footer>
-  <a
-    className="footer-link"
-    href="https://github.com/kitdog123/SheCodes-Weather-Project"
-  >
-    Open Source Code
-  </a>
-  by Kitty <br />
-  Hosted on
-  <a className="footer-link" href="https://www.netlify.com/">
-    Netlify
-  </a>
-  <br />
-  Animated icons by
-  <a
-    className="footer-link"
-    href="https://www.flaticon.com/free-animated-icons/weather"
-  >
-    Flat Icon
-  </a>
-</footer>;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 
 import Weather from "./Weather";
-import Search from "./Search";
 import Footer from "./Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Weather />
-
+    <Footer />
   </React.StrictMode>
 );
